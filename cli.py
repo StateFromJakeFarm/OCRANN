501d020194c34707b620595932156d21e6d771c6
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-05-03 11:48:08 -0700

    args

--- cli.py
+++ cli.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 
 from funcs import *
-from PIL import Image
-import sys
 import os
 import argparse
 
 def main():
+    # Parse command line args
     parser = argparse.ArgumentParser(prog='ocr')
-    parser.add_argument('-s', '--save', type=str, default=None,
-        help='save trained weights to provided filename')
     parser.add_argument('-i', '--iters', type=int, default=300,
         help='number of training iterations to run over provided training set (default is 300)')
-    parser.add_argument('-t', '--train', type=str, default=None,
+    parser.add_argument('-t', '--train', type=str, required=True,
         help='folder with training inputs')
-    parser.add_argument('-r', '--read', type=str, default=None,
+    parser.add_argument('-r', '--read', type=str, required=True,
         help='folder with images to read')
-    parser.add_argument('-s', '--structure', type=str, default=None,
+    parser.add_argument('-s', '--structure', type=str, required=True,
         help='ANN structure file')
     parser.add_argument('-w', '--weights', type=str, default='RANDOM',
         help='starting weights file (default is random starting weights)')
@@ -24,9 +21,16 @@ def main():
         help='encodings file (default is \"62encodings.txt\"')
     parser.add_argument('-a', '--alpha', type=float, default=0.01,
         help='the alpha value (default is 0.01)')
+    args = parser.parse_args()
 
-    parser.parse_args()
+    # Create train in/out files
+#    trainIn, trainOut = createTrainFiles(args.train)
+    # Create test (read) in/out files
+#    testIn, testOut = createTestFiles(args.read)
 
+    # Fire up the ann
+    ocrArgs = [str(value) for argName, value in vars(args).iteritems()]
+    print(repr(ocrArgs))
 
 if __name__ == '__main__':
     main()
